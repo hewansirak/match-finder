9cf7be7d5abee2fce8008ec7fd966980a9106d0d
Betemariam <betemariamabenet8468@gmail.com>
2025-06-30 15:46:34 +0300

    implemented quiz component along with the root file

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { useState } from "react";
 import "./App.css";
 import UserRegistration from "./components/UserRegistration";
 import type { UserData } from "./types/user";
+import Quiz from "./components/Quiz";
+import type { QuizAnswers } from "./types/quiz";
 
 function App() {
-  const [, setUserData] = useState<UserData | null>(null);
+  const [step, setStep] = useState<'registration' | 'quiz' | 'results' | 'no-match'>('registration');
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [, setQuizAnswers] = useState<QuizAnswers>({});
 
-  const handleRegistration = (userData: UserData) => {
-    setUserData(userData);
-    console.log("User registered:", userData);
+    const handleRegistration = (data: UserData) => {
+    setUserData(data);
+    setStep('quiz');
+  };
+
+  const handleQuizComplete = (answers: QuizAnswers) => {
+    setQuizAnswers(answers);
+    // console.log('Quiz Answers:', answers);
+    // console.log('User Data:', userData);
   };
   return (
-    <div>
-      <UserRegistration onComplete={handleRegistration} />
+    <div className="min-h-screen bg-gradient-to-br from-pink-100 via-red-50 to-rose-100">
+      {step === 'registration' && (
+        <UserRegistration onComplete={handleRegistration} />
+      )}
+      {step === 'quiz' && userData && (
+        <Quiz userData={userData} onComplete={handleQuizComplete} />
+      )}
     </div>
   );
 }
