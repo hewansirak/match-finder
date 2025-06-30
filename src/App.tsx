2db4a913c92a7e97e7e286417069dc96c28a579f
Betemariam <betemariamabenet8468@gmail.com>
2025-06-30 14:40:45 +0300

    fixed build error of unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import UserRegistration from "./components/UserRegistration";
 import type { UserData } from "./types/user";
